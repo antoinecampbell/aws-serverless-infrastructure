e2db18f827204a6b69ad2a7375e46239f9933d7e
Antoine Campbell <acampbell@captechventures.com>
2021-04-22 23:05:43 -0400

    - Updating performance tests

--- performance-tests/index.js
+++ performance-tests/index.js
@@ -4,7 +4,7 @@ import {check, sleep} from 'k6';
 export const options = {
   thresholds: {
     http_req_failed: ['rate<0.01'],   // http errors should be less than 1%
-    http_req_duration: ['p(95)<250'], // 95% of requests should be below 250ms
+    http_req_duration: ['p(95)<350'], // 95% of requests should be below 350ms
   }
 }
 
