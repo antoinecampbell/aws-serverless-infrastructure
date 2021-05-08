b83c743b822960db276e08a00431882aa80623d9
Antoine Campbell <acampbell@captechventures.com>
2021-05-07 20:45:00 -0400

    Added API alerts and lambda error alerts - Added alerts to the dashboard - Added threshold lines to graphs

--- terraform/resources/monitoring.tf
+++ terraform/resources/monitoring.tf
@@ -15,6 +15,10 @@ resource "aws_cloudwatch_dashboard" "serverless_dashboard" {
     get_notes_function_name: module.get_notes_lambda.function_name
     create_note_function_name: module.create_note_lambda.function_name
     notes_table_write_alarm_arn: aws_cloudwatch_metric_alarm.notes_table_write.arn
+    create_note_lambda_error_alarm_arn: aws_cloudwatch_metric_alarm.create_note_lambda_error.arn
+    get_notes_lambda_error_alarm_arn: aws_cloudwatch_metric_alarm.get_notes_lambda_error.arn
+    notes_api_5xx_error_alarm_arn: aws_cloudwatch_metric_alarm.notes_api_5xx_error.arn
+    notes_api_4xx_error_alarm_arn: aws_cloudwatch_metric_alarm.notes_api_4xx_error.arn
     notes_api_name: aws_api_gateway_rest_api.note.name
   })
 }
@@ -30,5 +34,61 @@ resource "aws_cloudwatch_metric_alarm" "notes_table_write" {
   period = 60
   dimensions = {
     TableName: aws_dynamodb_table.notes.name
+  }
+}
+
+resource "aws_cloudwatch_metric_alarm" "create_note_lambda_error" {
+  alarm_name = "create-note-lambda-alarm-${var.environment}"
+  namespace = "AWS/Lambda"
+  metric_name = "Errors"
+  statistic = "Sum"
+  comparison_operator = "GreaterThanOrEqualToThreshold"
+  evaluation_periods = 1
+  threshold = 5
+  period = 60
+  dimensions = {
+    FunctionName: module.create_note_lambda.function_name
+  }
+}
+
+resource "aws_cloudwatch_metric_alarm" "get_notes_lambda_error" {
+  alarm_name = "get-notes-lambda-alarm-${var.environment}"
+  namespace = "AWS/Lambda"
+  metric_name = "Errors"
+  statistic = "Sum"
+  comparison_operator = "GreaterThanOrEqualToThreshold"
+  evaluation_periods = 1
+  threshold = 5
+  period = 60
+  dimensions = {
+    FunctionName: module.get_notes_lambda.function_name
+  }
+}
+
+resource "aws_cloudwatch_metric_alarm" "notes_api_5xx_error" {
+  alarm_name = "notes-api-5xx-error-alarm-${var.environment}"
+  namespace = "AWS/ApiGateway"
+  metric_name = "5XXError"
+  statistic = "Sum"
+  comparison_operator = "GreaterThanOrEqualToThreshold"
+  evaluation_periods = 1
+  threshold = 5
+  period = 60
+  dimensions = {
+    ApiName: aws_api_gateway_rest_api.note.name
+  }
+}
+
+resource "aws_cloudwatch_metric_alarm" "notes_api_4xx_error" {
+  alarm_name = "notes-api-4xx-error-alarm-${var.environment}"
+  namespace = "AWS/ApiGateway"
+  metric_name = "4XXError"
+  statistic = "Sum"
+  comparison_operator = "GreaterThanOrEqualToThreshold"
+  evaluation_periods = 1
+  threshold = 5
+  period = 60
+  dimensions = {
+    ApiName: aws_api_gateway_rest_api.note.name
   }
 }
